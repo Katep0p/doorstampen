7c574bf557c0950b4bd53564a84abd82fce0a5a1
Decker108 <1337decker@gmail.com>
2016-12-27 19:36:14 +0100

    Ändrade i index-html och server.py

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def readFile():
+    # läs fil och returnera
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    htmlFileAsString = readFile()
+    return htmlFileAsString
 
 if __name__ == "__main__":
     app.run()
